525a4c1b608ebdab1292480fc31e7a5717d32585
19Edwin92 <b_a_s_e@naver.com>
2023-03-04 14:04:58 +0900

    4week-prac1

---
+++ src/components/Usememo1.jsx
@@ -0,0 +1,45 @@
+import React, { useMemo, useState } from 'react'
+
+const hardCalurate = (number) => {
+  console.log('어려운 계산기');
+  for (let i=0; i<500000000; i++) {} // 단순하게 반복문이 5억번 실행될 때까지 기다리는 시간이라고 하자. 
+  return number+10000
+}
+
+
+const easyCalurate = (number) => {
+  console.log('짱쉬운 계산기');
+  return number+1
+}
+
+function Usememo1() {
+  const [hardNumber, setHardNumber] = useState(0)
+  const [easyNumber, setEasyNumber] = useState(0)
+  // const hardSum = hardCalurate(hardNumber)
+  const hardSum = useMemo(()=>{
+    return hardCalurate(hardNumber)
+  }, [hardNumber])
+
+  const easySum = easyCalurate(easyNumber)
+  return (
+    <div>
+      <h3>어려운 계산기</h3>
+      <input 
+          type="number"
+          value={hardNumber}
+          onChange={(e)=> setHardNumber(Number(e.target.value))}
+          step="1"/>
+      <span>+10000 = {hardSum}</span>
+      <hr />
+      <h3>짱쉬운 계산기</h3>
+      <input 
+          type="number"
+          value={easyNumber}
+          onChange={(e)=> setEasyNumber(Number(e.target.value))}
+          step="1"/>
+      <span>+1 = {easySum}</span>
+    </div>
+  )
+}
+
+export default Usememo1
---
+++ src/components/Usememo2.jsx
@@ -0,0 +1,12 @@
+import React, { useMemo, useState } from 'react'
+
+function Usememo2() {
+
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  )
+}
+
+export default Usememo2
